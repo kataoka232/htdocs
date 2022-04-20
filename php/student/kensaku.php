bdfbe47e991cdb0e686ab26aea0a291f3846a889
unknown <teraoka_yu@atmarksol.jp>
2022-04-20 10:50:57 +0900

    生徒検索画面

---
+++ php/student/kensaku.php
@@ -0,0 +1,61 @@
+﻿
+<table>
+<tr><th>生徒IDを入力してください</tr></th>
+
+<form action="kensaku.php" method="post">
+	<tr><td>
+		<input type="text"name="id">
+		<input type="submit"value="検索">
+	</tr></td>
+
+<tr><th>選択コースを選んでください</tr></th>
+	<tr><td>
+		<input type="text"name="course">
+		<input type="submit"value="検索">
+	</tr></td>
+</form>
+
+<?php
+
+//error_reporting(0);
+
+$pdo=new PDO("mysql:host=localhost;dbname=juku;charset=utf8","root","");
+if(!empty($_REQUEST["id"]))
+	{
+		$select=$pdo->prepare("select * from student where studentid=:studentid");
+		$select->bindValue(":studentid",$_REQUEST["id"],PDO::PARAM_STR);
+		$select->execute();
+}else if(!empty($_REQUEST["course"]))
+	{
+		$select=$pdo->prepare("select * from student where course_id=:course_id");
+		$select->bindValue(":course_id",$_REQUEST["course"],PDO::PARAM_STR);
+		$select->execute();
+	
+}else if(empty($_REQUEST["course"])&&empty($_REQUEST["id"]))
+	{	
+		$sql = "select * from student where delete_flag = 0";
+		$select=$pdo->prepare($sql);
+		$select->execute();	
+	
+		/*$select=$pdo->prepare("select * from student");
+		$select->bindValue(":student",null,PDO::PARAM_NULL);
+		$select->execute();*/
+	}
+
+$result=$select->fetchAll();
+//var_dump($_REQUEST["course"]);
+foreach( $result as $row)
+	{
+		echo"<tr>";
+		echo "<td>",$row['studentid'], ":","</td>";	
+		echo "<td>",$row['course_id'], ":","</td>";
+		echo "<td>",$row['name'],"</td>";
+		echo "</tr>";
+
+	
+	}
+
+//echo print_r($result,true);
+
+?>
+</table>
---
+++ php/student/seitokensaku.php
@@ -0,0 +1,58 @@
+﻿<table>
+<tr><th></th></tr>
+<tr><td></td></tr>
+
+<tr><th>生徒検索</th></tr>
+<form action="seitokensaku.php" method="post">
+	<tr><th>
+	名前
+	<td><input type="text" name="name"></td>
+	<td><input type="submit" value="検索"></td>
+	</th></tr>
+	<tr><th>
+	フリガナ
+	<td><input type="text" name="furigana"></td>
+	<td><input type="submit" value="検索"></td>
+	</tr></th>
+	<tr><th>住所
+	<td><input type="text" name="address"></td>
+	<td><input type="submit" value="検索"></td>
+	</tr></th>
+	<tr><th>電話
+	<td><input type="text" name="tell"></td>
+	<td><input type="submit" value="検索"></td>
+	</tr></th>
+	<tr><th>緊急連絡先
+	<td><input type="text" name="emargency"></td>
+	<td><input type="submit" value="検索"></td>
+	</tr></th>
+	<tr><th>学年
+	<td><input type="text" name="year"></td>
+	<td><input type="submit" value="検索"></td>
+	</tr></th>
+	<tr><th>性別
+	<td><select name="sex">
+	<?php
+	for ($i=1;$i<3;$i++){
+		$pdo new PDO("mysql:host=localhost;dbname=juku;charset=utf8","root","");
+		echo '<option value="',$i,'">',$i,'</option>';
+	}
+	?>
+	</select>
+	</td>
+	<td><input type="submit" value="検索"></td>
+	</tr></th>
+	<tr><th>生年月日
+	<td><input type="date" name="birthday"></td>
+	<td><input type="submit" value="検索"></td>
+	</tr></th>
+	<tr><th>ID
+	<td><input type="text" name="id"></td>
+	<td><input type="submit" value="検索"></td>
+	</tr></th>
+	<tr><th>受講コース
+	<td><input type="text" name="course"></td>
+	<td><input type="submit" value="検索"></td>
+	</tr></th>
+
+</form>
