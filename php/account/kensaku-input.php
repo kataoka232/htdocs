a4d5e15ea26e5a9078d3fafbd3d3d9b194c51a54
Sugawara-takumu <103548981+Sugawara-takumu@users.noreply.github.com>
2022-04-20 11:26:21 +0900

    Revert "アカウント関連のphpです。"

--- php/account/kensaku-input.php
+++
@@ -1,7 +0,0 @@
-<?php require '../header.php'; ?>
-キーワードを入力してください。
-<form action="kensaku-output.php" method="post">
-<input type="text" name="keyword">
-<input type="submit" value="検索">
-</form>
-<?php require '../footer.php'; ?>
