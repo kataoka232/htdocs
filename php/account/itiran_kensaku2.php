d88105ccbef0faf5dcce1beb398f52a2e108a44f
sugawara <sugawara_ta@atmarksol.jp>
2022-04-25 13:04:40 +0900

    アカウント周り　2022/4/25

--- php/account/itiran_kensaku2.php
+++ php/account/itiran_kensaku2.php
@@ -2,14 +2,15 @@
 IDを入力してください。
 <html>
 <form action="itiran_kensaku2.php" method="post">
-<input type="text" name="account_id"><br>
+<input type="text" name="id"   value="<?= $_POST['id']?>"><br>
 アカウント名を入力してください。<br>
-<input type="text" name="account_name"><br>
+<input type="text" name="name" value="<?= $_POST['name']?>"><br>
 <input type="submit" value="検索"><br><br><br>
 <input type="hidden" name="search" value=1>
 
 
 <?php
+
 
 // エラー非表示
  //error_reporting(E_NOTICE);
@@ -20,12 +21,30 @@ IDを入力してください。
 
 $pdo=new PDO('mysql:host=localhost;dbname=juku;charset=utf8','root', '');
 
-if(!empty($_POST['account_id'])or!empty($_POST['account_name'])){
-	$stmt= $pdo->prepare('select * from account where id = ?  and name like ?');
-	$stmt->bindValue(1,     $_REQUEST['account_id']  ,PDO::PARAM_STR);
-	$stmt->bindValue(2, '%'.$_REQUEST['account_name'].'%',PDO::PARAM_STR);
-	$stmt->execute();
-	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if(!empty($_POST['id'])or!empty($_POST['name'])){
+	//$stmt= $pdo->prepare('select * from account where id = ?  and name like ?');
+	//$stmt->bindValue(1,     $_REQUEST['account_id']  ,PDO::PARAM_STR);
+	//$stmt->bindValue(2, '%'.$_REQUEST['account_name'].'%',PDO::PARAM_STR);
+	//$stmt->execute();
+    $sql="select * from account where delete_flg = 0 ";
+    $bind=[];
+
+    if(!empty($_REQUEST["id"])){
+             $sql.="and id = ?";
+             $bind[]=$_REQUEST["id"];
+    }
+
+    if(!empty($_REQUEST["name"])){
+             $sql.="and name like ?";
+             $bind[]='%'.$_REQUEST["name"].'%';
+    }
+
+    //$result=$select->fetchAll();
+    $sql = $pdo->prepare($sql);
+    $sql->execute($bind);
+    $result=$sql->fetchAll();
+
+	//$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 	print_r('<pre>');
 	print_r($result);
@@ -34,7 +53,7 @@ if(!empty($_POST['account_id'])or!empty($_POST['account_name'])){
 	//$sql=$pdo->prepare('select * from account where name like ?');
 	//$sql->execute(['%'.$_REQUEST['keyword'].'%']);
 
-	foreach ($stmt as $row) {
+	foreach ($result as $row) {
 		echo '<tr>';
 		echo '<td>', $row['id'], '</td>';
 		echo '<td>', $row['name'], '</td>';
@@ -52,14 +71,14 @@ if(!empty($_POST['account_id'])or!empty($_POST['account_name'])){
 	print_r($result);
 	print_r('</pre>');
 
-	foreach ($stmt as $row) {
+	foreach ($result as $row) {
 		echo '<tr>';
 		echo '<td>', $row['id'], '</td>';
 		echo '<td>', $row['name'], '</td>';
 		echo '<td>', $row['password'], '</td>',"<br>";
 		echo '</tr>';
 		echo "\n";
-	}
+	   }
 }
          
 //searchがあるかつキーワードがある時、検索する。
